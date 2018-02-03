6162835b9c2cb113c1ef7fad1bfa3944f83898ae
prashantbasawa <prashantbasawa@gmail.com>
2018-02-02 23:36:49 -0500

    Added some more validations & a demo class.

---
+++ src/main/java/com/pb/usecase/Demo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.pb.usecase;
+
+import java.util.Collection;
+
+import com.pb.validation.ValidationResult;
+
+/**
+ * @author prashantbasawa
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		
+		Collection<ValidationResult> errors = EmployeeValidationHelper.validate(employee);
+		errors.stream().forEach(e -> System.out.println(e.getReason().get()));
+	}
+}
--- src/main/java/com/pb/usecase/EmployeeValidationHelper.java
+++ src/main/java/com/pb/usecase/EmployeeValidationHelper.java
@@ -3,11 +3,16 @@
  */
 package com.pb.usecase;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
 import com.pb.validation.EmptyTextValidation;
+import com.pb.validation.NumericTextValidation;
+import com.pb.validation.TextLengthValidation;
 import com.pb.validation.ValidationInterface;
+import com.pb.validation.ValidationResult;
 
 /**
  * @author Prashant Basawa
@@ -15,11 +20,16 @@ import com.pb.validation.ValidationInterface;
  */
 public interface EmployeeValidationHelper {
 	
+	static Collection<ValidationResult> validate(Employee employee) {
+		return employeeValidations().stream().map(v -> v.test(employee)).collect(toList());
+	}
+	
 	static Collection<ValidationInterface<Employee>> employeeValidations() {
 		Collection<ValidationInterface<Employee>> validations = new ArrayList<>();
 		
 		validations.add(firstNameNotEmpty());
 		validations.add(lastNameNotEmpty());
+		validations.add(zipCodeNotEmpty().and(zipCode5CharsLong()).and(zipCodeNumeric()));
 		
 		return validations;
 	}
@@ -30,5 +40,17 @@ public interface EmployeeValidationHelper {
 	
 	static ValidationInterface<Employee> lastNameNotEmpty() {
 		return new EmptyTextValidation<>(Employee::getLastName, "Last name");
+	}
+	
+	static ValidationInterface<Employee> zipCodeNotEmpty() {
+		return new EmptyTextValidation<>(Employee::getZipCode, "Zip Code");
+	}
+	
+	static ValidationInterface<Employee> zipCodeNumeric() {
+		return new NumericTextValidation<>(Employee::getZipCode, "Zip Code");
+	}
+	
+	static ValidationInterface<Employee> zipCode5CharsLong() {
+		return new TextLengthValidation<Employee>(Employee::getZipCode, "Zip Code", 5);
 	}
 }
